5ab7142b9219ad34f71394ccb56f52fa4e1c1f1f
tuanlong1202 <tuanlong1202@gmail.com>
2021-09-02 11:15:07 -0400

    Phase 4 rails create read lab

---
+++ app/controllers/plant_controller.rb
@@ -0,0 +1,29 @@
+class PlantController < ApplicationController
+
+    def index
+        plants = Plant.all
+        render json: plants 
+    end
+
+    def show
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            render json: plant
+        else
+            render json: { error: "Plant not found" }, status: :not_found
+        end
+    end
+
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+
+    private
+    # all methods below here are private
+  
+    def plant_params
+      params.permit(:name, :image, :price)
+    end
+  
+end
